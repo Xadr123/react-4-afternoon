5c5b03d67bfa3b4876ae6c8887ee6dfcf5062576
Xadr123 <hansenxander@gmail.com>
2020-02-13 15:38:14 -0700

    completed black diamond

--- src/components/Student/Student.js
+++ src/components/Student/Student.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
+import Axios from 'axios';
+import { Link, Switch, Route } from 'react-router-dom'
 
 export default class Student extends Component {
   constructor() {
     super()
 
+    this.state = {
+      studentInfo: {}
+    }
+  }
+
+  componentDidMount() {
+    return Axios.get(`http://localhost:3005/students/${this.props.match.params.id}`).then(res => {
+      this.setState({
+        studentInfo: res.data
+      })
+    })
   }
 
   render() {
     return (
       <div className="box">
         <h1>Student</h1>
-      </div>
+        <h1>{this.state.studentInfo.first_name} {this.state.studentInfo.last_name}</h1>
+        <h3>Grade: {this.state.studentInfo.grade}</h3>
+        <h3>Email: {this.state.studentInfo.email}</h3>
+        <Link to={`/classlist/${this.state.studentInfo.class}`}>
+          <h3>Back</h3>
+        </Link>
+      </div >
     )
   }
 }
